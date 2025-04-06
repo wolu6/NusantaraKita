22d0e7f156cbd5e4c3ba734909cde2777214e715
Yuefii <muffidahmadiawan31@gmail.com>
2025-04-06 10:32:51 +0700

    chore: create api to get all provinsi

--- api/routes.ts
+++ api/routes.ts
@@ -1,13 +1,16 @@
 import { Request, Response, Router } from 'express';
 import ProvinsiControllers from './module/controllers/provinsi-controller';
+import KabupatenKotaControllers from './module/controllers/kabupaten-kota-controller';
 
 const routes = Router();
-const data = new ProvinsiControllers();
+const provinsi = new ProvinsiControllers();
+const kabupatenKota = new KabupatenKotaControllers();
 
 routes.get('/', (req: Request, res: Response): void => {
 	res.status(200).json({ message: 'Welcome to the API' });
 });
 
-routes.get('/provinsi', (req: Request, res: Response) => data.Get(req, res));
+routes.get('/provinsi', (req: Request, res: Response) => provinsi.Get(req, res));
+routes.get('/kab-kota', (req: Request, res: Response) => kabupatenKota.Get(req, res));
 
 export default routes;
