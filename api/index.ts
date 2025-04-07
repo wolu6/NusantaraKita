94dfa9f642e028f5ce2067aaf50fe07745b06ee7
Yuefii <muffidahmadiawan31@gmail.com>
2025-04-07 15:52:40 +0700

    chore: adding cors origin

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,7 @@ import morgan from 'morgan';
 import routes from './routes';
 import prisma from './lib/prisma/config';
 import express from 'express';
+import cors from 'cors';
 
 class App {
   public server;
@@ -17,6 +18,12 @@ class App {
   middlewares() {
     this.server.use(morgan('dev'));
     this.server.use(express.json());
+    this.server.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      next();
+    });
   }
 
   routes() {
