4fb1cfe7c7dd871d567081c3a794315c3fe4b079
Dillah Codes <abijuniansyah@gmail.com>
2025-06-10 21:39:04 +0700

    feat: enhance ProvinsiDisplayData component with mobile support and improved data rendering

--- docs/src/components/provinsi/provinsi-display-data.tsx
+++ docs/src/components/provinsi/provinsi-display-data.tsx
@@ -1,11 +1,15 @@
+import { useIsMobile } from "@/hooks/use-mobile";
 import { useMemo } from "react";
+import { IoIosCompass, IoIosInformationCircle, IoIosLocate, IoIosPin } from "react-icons/io";
+import { MdErrorOutline } from "react-icons/md";
+import { Alert, AlertDescription, AlertTitle } from "../ui/alert";
+import { CardList } from "../ui/card-list";
+import Code from "../ui/code";
 import { useResponseType } from "../ui/response-type";
 import SkeletonTable from "../ui/skelekton-table";
 import { Skeleton } from "../ui/skeleton";
-import { Alert, AlertDescription, AlertTitle } from "../ui/alert";
-import { MdErrorOutline } from "react-icons/md";
 import Table from "../ui/table";
-import Code from "../ui/code";
+import { CardListSkeleton } from "../ui/skelekton-card-list";
 
 interface ProvinsiDataProps {
   data: ProvinsiApiRes | undefined;
@@ -23,6 +27,29 @@ const parseProvinsi = (provinsi: ProvinsiApi) => ({
   Latitude: provinsi.lat,
   Longitude: provinsi.lng,
 });
+
+const parseProvinsiCard = (provinsi: ProvinsiApi) => [
+  {
+    icon: IoIosInformationCircle,
+    title: "Kode",
+    value: provinsi.kode.toString(),
+  },
+  {
+    icon: IoIosPin,
+    title: "Provinsi",
+    value: provinsi.nama,
+  },
+  {
+    icon: IoIosLocate,
+    title: "Latitude",
+    value: provinsi.lat.toString(),
+  },
+  {
+    icon: IoIosCompass,
+    title: "Longitude",
+    value: provinsi.lng.toString(),
+  },
+];
 
 const ProvinsiErrorAlert = () => {
   return (
@@ -46,30 +73,29 @@ const ProvinsiErrorAlert = () => {
 };
 
 export const ProvinsiDisplayData = ({ data, isError, isLoading, isSuccess, isPending }: ProvinsiDataProps) => {
+  const isMobile = useIsMobile();
+
   const { state } = useResponseType();
   const isShowData = isSuccess && data?.data;
 
+  const parsedProvinsiCard = useMemo(() => (!data?.data ? [] : data.data.map(parseProvinsiCard)), [data]);
+  const parsedProcinsiTable = useMemo(() => (!data?.data ? [] : data.data.map(parseProvinsi)), [data]);
+
   const renderData = useMemo(() => {
-    if ((isLoading || isPending) && state.dataType === "TABLE") {
-      return <SkeletonTable columnCount={5} rowCount={5} />;
-    }
+    const isTableShowData = isShowData && state.dataType === "TABLE";
 
-    if ((isLoading || isPending) && state.dataType === "JSON") {
-      return <Skeleton className="h-50" />;
-    }
+    if ((isLoading || isPending) && !isMobile) return <SkeletonTable columnCount={5} rowCount={5} />;
+    if ((isLoading || isPending) && isMobile) return <CardListSkeleton count={5} />;
 
+    if ((isLoading || isPending) && state.dataType === "JSON") return <Skeleton className="h-50" />;
     if (isError) return <ProvinsiErrorAlert />;
 
-    if (isShowData && state.dataType === "TABLE") {
-      return <Table columnHeaders={provinsiTableHeaders} rowData={data.data.map(parseProvinsi)} />;
-    }
+    if (isTableShowData && isMobile) return <CardList listData={parsedProvinsiCard} />;
+    if (isTableShowData) return <Table columnHeaders={provinsiTableHeaders} rowData={parsedProcinsiTable} />;
 
-    if (isShowData && state.dataType === "JSON") {
-      return <Code content={JSON.stringify(data, null, 2)} showCopyButton />;
-    }
-
+    if (isShowData && state.dataType === "JSON") return <Code content={JSON.stringify(data, null, 2)} showCopyButton />;
     return null;
-  }, [data, isShowData, state.dataType, isLoading, isPending, isError]);
+  }, [data, isShowData, state.dataType, isLoading, isPending, isError, isMobile, parsedProcinsiTable, parsedProvinsiCard]);
 
   return <div>{renderData}</div>;
 };
