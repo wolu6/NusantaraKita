75a97792f9d3f1e2be08da751b9ec9ef86d89564
Dillah Codes <abijuniansyah@gmail.com>
2025-06-11 01:16:17 +0700

    fix: update description text for DesaKel, Kabupaten/Kota, and Kecamatan components for consistency

--- docs/src/components/data-wilayah/desa-kelurahan/desa-kel-description.tsx
+++ docs/src/components/data-wilayah/desa-kelurahan/desa-kel-description.tsx
@@ -1,7 +1,9 @@
 const DesaKelDescription = () => (
   <div className="mb-10">
     <h2 className="text-xl font-semibold mb-4 text-gray-600">Description</h2>
-    <p className="text-gray-600 leading-relaxed">Menampilkan daftar data desa kelurahan di Indonesia, Dari Sabang sampai Merauke 🇮🇩</p>
+    <p className="text-gray-600 leading-relaxed">
+      Menampilkan daftar data desa kelurahan di Indonesia, Dari Sabang sampai Merauke &#127470;&#127465;
+    </p>
   </div>
 );
 
--- docs/src/components/data-wilayah/kecamatan/kecamatan-description.tsx
+++ docs/src/components/data-wilayah/kecamatan/kecamatan-description.tsx
@@ -1,6 +1,8 @@
 export const KecamatanDescription = () => (
   <div className="mb-10">
     <h2 className="text-xl font-semibold mb-4 text-gray-600">Description</h2>
-    <p className="text-gray-600 leading-relaxed">Menampilkan daftar data kecamatan di Indonesia, Dari Sabang sampai Merauke 🇮🇩</p>
+    <p className="text-gray-600 leading-relaxed">
+      Menampilkan daftar data kecamatan di Indonesia, Dari Sabang sampai Merauke &#127470;&#127465;
+    </p>
   </div>
 );
