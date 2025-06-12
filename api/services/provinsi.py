8e74038359362ca254af595c195512598be11713
Yuefii <muffidahmadiawan31@gmail.com>
2025-06-12 12:32:18 +0700

    chore: adding type hinting for provinsi

--- api/services/provinsi.py
+++ api/services/provinsi.py
@@ -1,16 +1,17 @@
 import aiomysql
-
+from typing import List, Union
 from config import get_connection
+from models.provinsi import Provinsi, ProvinsiListResponse, PaginatedProvinsiResponse
 
 
 class ProvinsiService:
-    async def get(self, limit: int, halaman: int, pagination: bool):
+    async def get(self, limit: int, halaman: int, pagination: bool) -> Union[ProvinsiListResponse, PaginatedProvinsiResponse]:
         conn = await get_connection()
         async with conn.cursor(aiomysql.DictCursor) as cursor:
             try:
                 if not pagination:
                     await cursor.execute("SELECT * FROM nk_provinsi")
-                    data = await cursor.fetchall()
+                    data: List[Provinsi] = await cursor.fetchall()
                     if not data:
                         raise Exception("tidak ditemukan data")
                     return {"data": data}
@@ -21,24 +22,24 @@ class ProvinsiService:
                     )
 
                 await cursor.execute("SELECT COUNT(*) AS total FROM nk_provinsi")
-                total_item = (await cursor.fetchone())["total"]
-                total_halaman = -(-total_item // limit)
+                total_item: int = (await cursor.fetchone())["total"]
+                total_halaman: int = -(-total_item // limit)
 
                 if halaman > total_halaman:
                     raise Exception(
                         f"nomor halaman melebihi total halaman. Halaman maksimum adalah {total_halaman}"
                     )
 
-                offset = (halaman - 1) * limit
+                offset: int = (halaman - 1) * limit
                 await cursor.execute(
                     "SELECT * FROM nk_provinsi LIMIT %s OFFSET %s", (limit, offset)
                 )
-                data = await cursor.fetchall()
+                data: List[Provinsi] = await cursor.fetchall()
 
                 if not data:
                     raise Exception("tidak ditemukan data untuk halaman yang diminta")
 
-                result = {
+                return {
                     "pagination": {
                         "total_item": total_item,
                         "total_halaman": total_halaman,
@@ -47,7 +48,5 @@ class ProvinsiService:
                     },
                     "data": data,
                 }
-
-                return result
             finally:
                 conn.close()
