87e52bfae6eaa7d5f26be7dcbca7f631cf5bd456
Yuefii <muffidahmadiawan31@gmail.com>
2025-06-12 13:29:20 +0700

    chore: adding docstring for model desa/kelurahan

--- api/models/desa_kelurahan.py
+++ api/models/desa_kelurahan.py
@@ -2,6 +2,16 @@ from typing import TypedDict, List
 from .pagination import PaginationMeta
 
 class DesaKelurahan(TypedDict):
+    """
+    Struktur data untuk response daftar desa/kelurahan.
+
+    Attributes:
+        kode (str): Kode unik/primary key desa/kelurahan.
+        nama (str): Nama desa/kelurahan.
+        lat (float): Latitude dari lokasi desa/kelurahan.
+        lng (float): Longitude dari lokasi desa/kelurahan.
+        kode_kecamatan (str): Kode kecamatan tempat desa/kelurahan berada.
+    """
     kode: str
     nama: str
     lat: float
@@ -9,8 +19,21 @@ class DesaKelurahan(TypedDict):
     kode_kecamatan: str
 
 class PaginatedDesaKelurahanResponse(TypedDict):
+    """
+    Struktur data untuk response daftar desa/kelurahan dengan informasi pagination.
+
+    Attributes:
+        pagination (PaginationMeta): Informasi metadata pagination.
+        data (List[DesaKelurahan]): Daftar desa/kelurahan.
+    """
     pagination: PaginationMeta
     data: List[DesaKelurahan]
 
 class DesaKelurahanListResponse(TypedDict):
+    """
+    Struktur data untuk response daftar desa/kelurahan tanpa informasi pagination.
+
+    Attributes:
+        data (List[DesaKelurahan]): Daftar desa/kelurahan.
+    """
     data: List[DesaKelurahan]
