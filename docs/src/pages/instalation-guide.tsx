d4d776698476eaf867e29c9585f0127c904ffd5c
Yuefii <muffidahmadiawan31@gmail.com>
2025-06-15 11:07:48 +0700

    chore: refact installation guide

--- docs/src/pages/instalation-guide.tsx
+++ docs/src/pages/instalation-guide.tsx
@@ -22,53 +22,76 @@ export const InstalationGuide = () => {
         "p-8": !isSmallScreen,
       })}
     >
-      <h1 className="text-3xl font-bold mb-10 text-gray-700">API Documentation - Installation Guide</h1>
+      <h1 className="text-3xl font-bold mb-10 text-gray-700">
+        API Documentation - Installation Guide
+      </h1>
 
       {/* Section Clone */}
       <div className="mb-10">
-        <h2 className="text-xl font-semibold mb-4 text-gray-600">Clone Repository</h2>
-        <p className="text-gray-600 leading-relaxed mb-4">Clone project NusantaraKita ke local kamu menggunakan perintah berikut:</p>
+        <h2 className="text-xl font-semibold mb-4 text-gray-600">
+          Clone Repository
+        </h2>
+        <p className="text-gray-600 leading-relaxed mb-4">
+          Clone project NusantaraKita ke local kamu menggunakan perintah
+          berikut:
+        </p>
         <Code content={cloneCommand} showCopyButton />
       </div>
 
       {/* Section Masuk API Directory */}
       <div className="mb-10">
-        <h2 className="text-xl font-semibold mb-4 text-gray-600">Masuk ke Direktori API</h2>
+        <h2 className="text-xl font-semibold mb-4 text-gray-600">
+          Masuk ke Direktori API
+        </h2>
         <Code content={`cd api`} showCopyButton />
       </div>
 
       {/* Section Setup Env */}
       <div className="mb-10">
-        <h2 className="text-xl font-semibold mb-4 text-gray-600">Setup Environment</h2>
-        <Code content={`cp env.example .env`} showCopyButton />
+        <h2 className="text-xl font-semibold mb-4 text-gray-600">
+          Setup Environment
+        </h2>
+        <Code content={`cp .env.example .env`} showCopyButton />
       </div>
 
       {/* Section Virtual Environment */}
       <div className="mb-10">
-        <h2 className="text-xl font-semibold mb-4 text-gray-600">Menjalankan Project dengan pip</h2>
+        <h2 className="text-xl font-semibold mb-4 text-gray-600">
+          Menjalankan Project dengan pip
+        </h2>
 
         <div className="mb-6">
-          <h3 className="text-lg font-medium mb-2 text-gray-600">Buat Virtual Environment</h3>
-          <Code content={`python3 -m venv venv`} showCopyButton />
+          <h3 className="text-lg font-medium mb-2 text-gray-600">
+            Buat Virtual Environment
+          </h3>
+          <Code content={`python -m venv venv`} showCopyButton />
         </div>
 
         <div className="mb-6">
-          <h3 className="text-lg font-medium mb-2 text-gray-600">Aktifkan Virtual Environment</h3>
+          <h3 className="text-lg font-medium mb-2 text-gray-600">
+            Aktifkan Virtual Environment
+          </h3>
           <Code content={`source venv/bin/activate`} showCopyButton />
         </div>
 
         <div className="mb-6">
-          <h3 className="text-lg font-medium mb-2 text-gray-600">Install Dependencies</h3>
+          <h3 className="text-lg font-medium mb-2 text-gray-600">
+            Install Dependencies
+          </h3>
           <Code content={`pip install -r requirements.txt`} showCopyButton />
         </div>
 
         <div className="mb-6">
-          <h3 className="text-lg font-medium mb-2 text-gray-600">Jalankan Project</h3>
+          <h3 className="text-lg font-medium mb-2 text-gray-600">
+            Jalankan Project
+          </h3>
           <Code content={`uvicorn generate:app --reload`} showCopyButton />
         </div>
 
         <div className="mb-6">
-          <h3 className="text-lg font-medium mb-2 text-gray-600">Testing Project</h3>
+          <h3 className="text-lg font-medium mb-2 text-gray-600">
+            Testing Project
+          </h3>
           <Code content={`pytest tests/`} showCopyButton />
         </div>
       </div>
